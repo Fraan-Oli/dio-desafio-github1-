0721aa2b4791167c88e8542397c770232076d096
Fraan-Oli <106706214+Fraan-Oli@users.noreply.github.com>
2022-06-05 21:47:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meu Primeiro Projeto </title>
+</head>
+
+<style>
+body {
+  background-color: #230f2b;
+  color: white;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+ 	color: white;
+  font-family: verdana;
+  font-size: 20px;
+
+  h2 {
+
+  }
+
+}
+</style>
+<body>
+
+	<h1>Como Iniciar na programação</h1>
+<p>Primeiro passos a seguir</p>
+
+
+<ul> Aprenda logica de Programação e seus fundamentos 
+</ul>
+ <ul>Aprenda sobre Git e GitHub</ul>
+ <ul>Aprenda sobre bancos de dados </ul>
+  <ul>Efim escolha uma Linguagem de Programação e se arrisque se desafie </ul>
+
+
+<h2> Explore Alguns sites que ensinam  </h2>
+
+
+<a href="https://web.dio.me/pro">
+	<button>Dio</button>
+
+	<br> </br>
+
+</a>
+
+	<a href="https://www.w3schools.com/">
+	<button>W3Schools</button>
+
+	<br> </br>
+</a>
+
+	Explore aprenda e compartilhe suas indicações !
+
+
+</body>
+</html>
